4f5d60da3fdf5163a91f3daa69d23df0bd497643
Steventhibe <steventhibe@gmail.com>
2023-02-04 23:41:19 +0800

    add annotation of useEffect and useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,107 @@
-import { useCallback, useEffect, useState } from "react"
-import HangmanDrawing from "./HangmanDrawing"
-import HangmanWord from "./HangmanWord"
-import Keyboard from "./Keyboard"
-import words from "./wordList.json"
+import { useCallback, useEffect, useState } from "react";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
+import words from "./wordList.json";
 
-function getWord(){
-  return words[Math.floor(Math.random() * words.length)]
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)];
 }
 
-
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(getWord)
-  const [guessedLetters, setGuesedLetters] = useState<string[]>([])
-  
+  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [guessedLetters, setGuesedLetters] = useState<string[]>([]);
+
   const incorrectLetters = guessedLetters.filter(
-    letter => !wordToGuess.includes(letter)
-  )
-  const isLoser = incorrectLetters.length >= 6
-  const isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter))
+    (letter) => !wordToGuess.includes(letter)
+  );
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
   const isGameGoing = !isLoser && !isWinner;
 
-    const addGuessedLetter = useCallback(
-      (letter: string) => {
-      if (guessedLetters.includes(letter) || isLoser || isWinner) return
+  // 透過 useCallback 我們可以紀錄這個函式的狀態，只會在第二個參數有變化時，重新定義此函式
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      console.log(guessedLetters);
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
 
-      setGuesedLetters(currentLetters => [...currentLetters, letter])
-      }
-    , [guessedLetters, isWinner, isLoser])
+      setGuesedLetters((currentLetters) => [...currentLetters, letter]);
+      console.log("set!");
+    },
+    [guessedLetters, isWinner, isLoser]
+  );
 
-    useEffect(() => {
-      const handler = (e: KeyboardEvent) => {
-        const key = e.key
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
 
-        if (!key.match(/^[a-z]$/)) return;
+      if (!key.match(/^[a-z]$/)) return;
 
-        e.preventDefault();
-        addGuessedLetter(key)
-      }
+      e.preventDefault();
+      console.log("run");
+      addGuessedLetter(key);
+    };
 
-      document.addEventListener("keypress", handler)
+    document.addEventListener("keypress", handler);
 
-      return ()=>{
-        document.removeEventListener("keypress", handler)
-      }
-    }, [guessedLetters])
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]); // 第二參數 guessedLetters 的設置使得 App.tsx 在 guessedLetters 改變時會更新，就不會出現輸入重複字母連續答錯畫線的情況
 
-    useEffect(()=>{
-      const handler = (e: KeyboardEvent) => {
-        const key = e.key
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
 
-        if(key !== "Enter") return
-        e.preventDefault();
-        setGuesedLetters([])
-        setWordToGuess(getWord())
-      }
+      if (key !== "Enter") return;
+      e.preventDefault();
+      setGuesedLetters([]);
+      setWordToGuess(getWord());
+    };
 
-      document.addEventListener("keypress", handler)
+    document.addEventListener("keypress", handler);
 
-      return ()=>{
-        document.removeEventListener("keypress", handler)
-      }
-    })
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  });
 
   return (
-    <div style={{maxWidth: "800px", display:"flex", flexDirection:"column", gap:"2rem", margin:"0 auto", alignItems:"center"}}>
-      <div style={{fontSize: "2rem", textAlign: "center"}}>
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
         {isGameGoing && "Press keyboard or click buttons"}
         {isWinner && "Winner! - Refresh to try again"}
         {isLoser && "Nice Try! - Refresh to try again"}
       </div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-      <HangmanWord reveal={isLoser} guessedLetters={guessedLetters} wordToGuess={wordToGuess}/>
-      <div style={{alignSelf: "stretch"}}>
-
-      <Keyboard disabled={isWinner || isLoser} activeLetters={guessedLetters.filter(letter => wordToGuess.includes(letter))} inactiveLetters={incorrectLetters} addGuessedLetter={addGuessedLetter}/>
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter((letter) =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
